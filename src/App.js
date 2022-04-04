ae930eee91dc8486c93eb7109c7c1e37c231f0bf
skymun471 <munhaneul2208@gmail.com>
2022-04-05 03:48:59 +0900

    React useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+// 함수를 사용해 app 컴포넌트를 생성한다.
 function App() {
-  return (
+  const [text, setText] = useState('Kossie');
+// -----------------------------------------------
+  const onSubmit = () => {
+    alert("submitted")
+  };
+// ------------------------------------------------
+  const onKeyUp = (event) => {
+    if(event.keyCode === 13){
+      onSubmit();
+    }
+    console.log('key up')
+  };
+// -----------------------------------------------
+  // let text = "Haneul"
+
+  const updateText =() => {
+    // text = 'Coder';
+    setText('Coder')
+    console.log(text)
+  };
+// -------------------------------------------------
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onKeyUp={onKeyUp}></input>
+      <button onClick={onSubmit}>submit</button>
+
+      <br/><br/>
+      <span>{text}</span>
+      <button onClick={updateText}>update</button>
     </div>
   );
 }
-
+// ------------------------------------------------
 export default App;
+
+// ()=> alert("submit") onClick내부에는 함수 형식이 들어가야한다.
+// onKeyUp 함수 내부에 event의 keyCode 값이 13(enter)일 때 onSubmit() 함수를 실행하도록 설정한다.
+// jsx에서 변수를 사용하고 싶을 때는 {} 중괄호 사이에 넣어 주면 된다.
+// useState를 활용하면 setText를 활용해 변수를 쉽게 변화 시킬 수 있다.
