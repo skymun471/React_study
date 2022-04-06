c66cbf7b6c3f4f6bbcc10fa157986e2263cad6b3
skymun471 <munhaneul2208@gmail.com>
2022-04-07 07:37:40 +0900

    useState use in form

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, {useState} from 'react';
 // 함수를 사용해 app 컴포넌트를 생성한다.
 function App() {
-  const [text, setText] = useState('Kossie');
-// -----------------------------------------------
-  const onSubmit = () => {
-    alert("submitted")
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
+    // alert("submitted")
   };
-// ------------------------------------------------
-  const onKeyUp = (event) => {
-    if(event.keyCode === 13){
-      onSubmit();
-    }
-    console.log('key up')
-  };
-// -----------------------------------------------
-  // let text = "Haneul"
-
-  const updateText =() => {
-    // text = 'Coder';
-    setText('Coder')
-    console.log(text)
-  };
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 // -------------------------------------------------
   return (  
     <div className="App">
-      <input onKeyUp={onKeyUp}></input>
-      <button onClick={onSubmit}>submit</button>
+      <form onSubmit={onSubmit}>
+        <input placeholder='Username'
+        value={username}
+        onChange={(e)=> setUsername(e.target.value)}
+        /><br/>
 
-      <br/><br/>
-      <span>{text}</span>
-      <button onClick={updateText}>update</button>
+        <input placeholder='Password' 
+        value={password}
+        onChange={(e)=> setPassword(e.target.value)}
+        /><br/>
+
+        <button>Login</button>
+      </form>
     </div>
   );
 }
@@ -40,3 +34,5 @@ export default App;
 // onKeyUp 함수 내부에 event의 keyCode 값이 13(enter)일 때 onSubmit() 함수를 실행하도록 설정한다.
 // jsx에서 변수를 사용하고 싶을 때는 {} 중괄호 사이에 넣어 주면 된다.
 // useState를 활용하면 setText를 활용해 변수를 쉽게 변화 시킬 수 있다.
+// onchange={} text 값이 바뀔 떄마다 이벤트를 보내준다.
+// onSubmit={} 형태는 이벤트 발동이다. {} 안에 함수를 실행 할 수 있다.
